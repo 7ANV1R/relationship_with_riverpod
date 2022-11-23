d8d2029b64bd09e331f4f1bf471a24d3e0f9956c
Tanvir <ibnmizan7878@gmail.com>
2022-11-24 03:10:21 +0600

    updated future provider

--- lib/future_provider.dart
+++ lib/future_provider.dart
@@ -1,14 +1,44 @@
+import 'package:all_provider/repo/api_repo.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final futureProviderProvider = FutureProvider(
+  (ref) async {
+    final apiService = ref.watch(apiServiceProvider);
+    return apiService.getSuggestion();
+  },
+);
 
 class FutureRivProvider extends ConsumerWidget {
   const FutureRivProvider({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    var futureProviderResult = ref.watch(futureProviderProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Future Provider'),
+      ),
+      body: RefreshIndicator(
+        onRefresh: () {
+          return ref.refresh(futureProviderProvider.future);
+        },
+        child: ListView(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            futureProviderResult.when(
+              data: (data) => Center(child: Text(data.activity)),
+              error: (error, stackTrace) => Text(
+                error.toString(),
+              ),
+              loading: () => const Center(
+                child: CircularProgressIndicator.adaptive(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/repo/api_repo.dart
+++ lib/repo/api_repo.dart
@@ -2,24 +2,28 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:all_provider/model/sug_model.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart';
 
 final apiServiceProvider = Provider(
   (ref) => ApiRepo(
-    dioClient: Dio(),
+    dioClient: Client(),
   ),
 );
 
 class ApiRepo {
-  final Dio _dioClient;
+  final Client _client;
 
-  ApiRepo({required Dio dioClient}) : _dioClient = dioClient;
+  ApiRepo({required Client dioClient}) : _client = dioClient;
 
   Future<Suggestion> getSuggestion() async {
     try {
-      var response = await _dioClient.get('https://www.boredapi.com/api/activity');
-      return Suggestion.fromJson(jsonDecode(response.data));
+      var response = await _client.get(Uri.parse('https://www.boredapi.com/api/activity'));
+      return Suggestion.fromJson(
+        jsonEncode(
+          jsonDecode(response.body),
+        ),
+      );
     } catch (e) {
       log(e.toString());
       throw Exception(e.toString());
